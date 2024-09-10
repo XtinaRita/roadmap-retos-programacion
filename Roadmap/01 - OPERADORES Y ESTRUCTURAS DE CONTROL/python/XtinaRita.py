c6add2a6834ec862ca45443343af3bbf6e6ca57e
Cris <62516454+XtinaRita@users.noreply.github.com>
2024-09-10 11:45:54 +0200

    XtinaRita.py

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/XtinaRita.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/XtinaRita.py
@@ -3,7 +3,8 @@ Operadores
 """
 
 # Operadores aritméticos
-print(f"Suma: 56 - 32 = {56 - 32}")
+print(f"Resta: 56 - 32 = {56 - 32}")
 print(f"Suma: 5 + 8 = {5 + 8}")
-print(f"Suma: 3 * 1 = {3 * 1}")
-print(f"Suma: 10 / 5 = {10 / 5}")
+print(f"Multiplicar: 3 * 1 = {3 * 1}")
+print(f"Dividir: 10 / 5 = {10 / 5}")
+
