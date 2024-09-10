7061dee81eeef312dcf464499488b817a3dcff0f
Cris <62516454+XtinaRita@users.noreply.github.com>
2024-09-10 14:58:05 +0200

    XtinaRita.py

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/XtinaRita.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/XtinaRita.py
@@ -7,4 +7,59 @@ print(f"Resta: 56 - 32 = {56 - 32}")
 print(f"Suma: 5 + 8 = {5 + 8}")
 print(f"Multiplicar: 3 * 1 = {3 * 1}")
 print(f"Dividir: 10 / 5 = {10 / 5}")
+print(f"Módulo: 15 % 4 = {15 % 4}")
+print(f"Exponente: 5 ** 3 = {5 ** 3}")
+print(f"División entera: 10 // 4 = {10 // 4}")
+
+# Operadores de comparación
+print(f"Igualdad: 8 == 4 es {8 == 4}")
+print(f"Desigualidad: 8 != 4 es {8 != 4}")
+print(f"Mayor que: 8 > 4 es {8 > 4}")
+print(f"Menor que: 8 < 4 es {8 < 4}")
+print(f"Mayor o igual que: 8 >= 8 es {8 >= 8}")
+print(f"Menor o iguaL que: 8 <= 4 es {8 <= 4}")
+
+# Operadores lógicos
+print(f"AND &&: 5 + 5 == 10 and 5 - 1 == 4 es {5 + 5 == 10 and 5 - 1 == 4}")
+print(f"OR ||: 5 + 5 == 10 or 5 - 1 == 4 es {5 + 5 == 8 or 5 - 1 == 2}")
+print(f"NOT !: not 5 + 5 == 8 es {not 5 + 5 == 8}")
+
+# Operadores de asignación
+my_num = 4 # asignación
+print(my_num)
+my_num += 1 # suma y asignación
+print(my_num) # asignación de +1 a cada print
+print(my_num) # 6
+print(my_num) # 7...
+my_num -= 1 # resta y asignación
+my_num *= 1 # multiplicación y asignación
+my_num /= 1 # división y asignación
+my_num %= 1 # módulo y asignación
+my_num //= 1 # división entera y asignación
+my_num **= 1 # exponente y asignación
+
+# Operadores de identidad
+my_number = 6.0
+print(f"my_num is my_number es {my_num is my_number}")
+my_number = my_num
+print(f"my_num is my_number es {my_num is my_number}")
+print(f"my_num is not my_number es {my_num is not my_number}")
+
+# Operadores de pertenencia
+print(f"'i' in 'cristina' = {'i' in 'cristina'}")
+print(f"'b' not in 'cristina' = {'b' not in 'cristina'}")
+
+# Operadores de bit
+
+
+
+
+
+
+
+
+
+
+
+
 
